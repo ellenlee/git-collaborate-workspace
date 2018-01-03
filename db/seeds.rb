868edc85ff1ed7eed82227eca6e7dd25bc998e2d
Ellen Lee <leechinghui.tw@gmail.com>
2018-01-03 17:17:51 +0800

    user index page and seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create!(email: "leechinghui.tw@gmail.com", password: "12345678")
+User.create!(email: "kevinwu.user1@gmail.com", password: "12345678")
